a45048f36937548b07b9c5a8c83211357cda91c4
Josh Tan <joshcx.tan@gmail.com>
2021-05-05 15:34:53 +0800

    Updated script to publaynet.

---
+++ scripts/train_pubnet.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ../tools
+
+python train_net.py \
+    --dataset_name          publaynet-layout \
+    --json_annotation_train ../data/publaynet/annotations/annotations-train.json \
+    --image_path_train      ../data/publaynet/raw/train \
+    --json_annotation_val   ../data/publaynet/annotations-val.json \
+    --image_path_val        ../data/publaynet/raw/val \
+    --config-file           ../configs/publaynet/mask_rcnn_R_50_FPN_3x.yaml \
+    --resume \
+    OUTPUT_DIR  ../outputs/publaynet/mask_rcnn_R_50_FPN_3x/ \
+    SOLVER.IMS_PER_BATCH 2 
