97851e321b7378d93e704facf2cc06e3f42534cc
Josh Tan <joshcx.tan@gmail.com>
2021-05-17 14:20:56 +0800

    Update train_publaynet.sh

--- scripts/train_publaynet.sh
+++ scripts/train_publaynet.sh
@@ -8,6 +8,6 @@ python train_net.py \
     --image_path_train      ../data/publaynet/raw/train \
     --json_annotation_val   ../data/publaynet/annotations-val.json \
     --image_path_val        ../data/publaynet/raw/val \
-    --config-file           ../configs/publaynet/faster_rcnn_R_50_FPN_3x.yml \
-    OUTPUT_DIR  ../outputs/publaynet/faster_rcnn_R_50_FPN_3x.yml/ \
+    --config-file           ../configs/publaynet/mask_rcnn_X_101_32x8d_FPN_3x \
+    OUTPUT_DIR  ../outputs/publaynet/mask_rcnn_X_101_32x8d_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
