5156484b8937d3fb1d6ee70c89d3a2acb969f4de
Josh Tan <joshcx.tan@gmail.com>
2021-05-20 09:50:23 +0800

    Update train_publaynet.sh

--- scripts/train_publaynet.sh
+++ scripts/train_publaynet.sh
@@ -6,7 +6,7 @@ python train_net.py \
     --dataset_name          publaynet \
     --json_annotation_train ../data/publaynet/annotations/annotations-train.json \
     --image_path_train      ../data/publaynet/raw/train \
-    --json_annotation_val   ../data/publaynet/annotations-val.json \
+    --json_annotation_val   ../data/publaynet/annotations/annotations-val.json \
     --image_path_val        ../data/publaynet/raw/val \
     --config-file           ../configs/publaynet/mask_rcnn_X_101_32x8d_FPN_3x.yaml \
     OUTPUT_DIR  ../outputs/publaynet/mask_rcnn_X_101_32x8d_FPN_3x/ \
